ed4eacc069d24adae1537df1b30269d89aeb7169
MadTom-Zh <m13_______91@163.com>
2024-08-12 10:35:23 +0800

    Delete App&Tools/SemiAutoSerialCom directory

--- App&Tools/SemiAutoSerialCom/Translators/ITranslator/ITranslator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MadTomDev.App.Translators
-{
-    public interface ITranslator
-    {
-        string Name { get; }
-        string ToUser(string machineMessage, out bool seccess);
-        string ToMachine(string userMessage, out bool seccess);
-    }
-}
--- App&Tools/SemiAutoSerialCom/copy translators.bat
+++
@@ -1,3 +0,0 @@
-
-copy Translators\PicoQATester_en\bin\Debug\net7.0\PicoQATester_en.dll SemiAutoSerialCom\bin\debug\net7.0-windows\Translators
-copy Translators\PicoQATester_zh\bin\Debug\net7.0\PicoQATester_zh.dll SemiAutoSerialCom\bin\debug\net7.0-windows\Translators
