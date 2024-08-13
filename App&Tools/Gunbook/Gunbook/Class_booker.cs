94e30bef9841b3b30c765605813975af3ab08a1f
MadTom-Zh <m13_______91@163.com>
2024-08-13 09:30:32 +0800

    Add files via upload

---
+++ App&Tools/Gunbook/Gunbook/Class_booker.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Controls;
+using System.IO;
+using System.Web;
+using System.Windows.Media.Imaging;
+
+namespace Gunbook
+{
+    public class Class_booker
+    {
+        public class CatalogEntry
+        {
+            public Class_booker parent;
+            private string _DirPath;
+            public string DirPath
+            {
+                set
+                {
+                    _DirPath = value;
+                    button.Click += Button_Click;
+                    LoadAllPics();
+                }
+                get
+                {
+                    return _DirPath;
+                }
+            }
+
+            private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+            {
+                if (parent.CatalogEntry_Clicked != null)
+                {
+                    parent.CatalogEntry_Clicked(parent, new CatalogEntry_ClickedArgs(this));
+                }
+            }
+
+            public string Text
+            {
+                set
+                {
+                    button.Content = value;
+                }
+            }
+            public Button button = new Button()
+            {
+                Height = 40,
+            };
+            public class gunEntry
+            {
+                public CatalogEntry parent;
+                public Image image = new Image();
+                public FileInfo imageFileInfo
+                {
+                    set
+                    {
+                        BitmapImage bitmap = new BitmapImage();
+                        bitmap.BeginInit();
+                        bitmap.UriSource = new Uri(value.FullName);
+                        bitmap.EndInit();
+                        image.Source = bitmap;
+                        image.Width = bitmap.PixelWidth;
+                        image.Height = bitmap.PixelHeight;
+                    }
+                }
+            }
+
+
+            public List<gunEntry> guns = new List<gunEntry>();
+            private void LoadAllPics()
+            {
+                gunEntry gun;
+                DirectoryInfo di = new DirectoryInfo(_DirPath);
+                foreach (FileInfo fi in di.GetFiles("*.png"))
+                {
+                    gun = new gunEntry()
+                    {
+                        parent = this,
+                        imageFileInfo = fi,
+                    };
+                    guns.Add(gun);
+                }
+
+
+            }
+        }
+
+
+        public List<CatalogEntry> catalog = new List<CatalogEntry>();
+        public Class_booker(DirectoryInfo bookDir)
+        {
+            CatalogEntry cEntry;
+            foreach (DirectoryInfo di in bookDir.GetDirectories())
+            {
+                cEntry = new CatalogEntry()
+                {
+                    parent = this,
+                    Text = HttpUtility.UrlDecode(di.Name),
+                    DirPath = di.FullName,
+                };
+                catalog.Add(cEntry);
+            }
+        }
+        public class CatalogEntry_ClickedArgs : EventArgs
+        {
+            public CatalogEntry targetCatalogEntry;
+            public CatalogEntry_ClickedArgs(CatalogEntry targetCatalogEntry)
+            {
+                this.targetCatalogEntry = targetCatalogEntry;
+            }
+        }
+        public event EventHandler<CatalogEntry_ClickedArgs> CatalogEntry_Clicked;
+    }
+}
---
+++ App&Tools/Gunbook/Gunbook/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+
+namespace Gunbook
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ReloadPictures();
+            ReSize_wrapPanel_main(new Size(scrollViewer_main.Width, scrollViewer_main.Height));
+        }
+
+        private void scrollViewer_main_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ReSize_wrapPanel_main(e.NewSize);
+        }
+        private void ReSize_wrapPanel_main(Size parentSize)
+        {
+            double newWidth = scrollViewer_main.ActualWidth - 30;
+            wrapPanel_main.Width = newWidth;
+            int items = wrapPanel_main.Children.Count;
+
+            if (items == 0)
+            {
+                wrapPanel_main.Height = 30;
+            }
+            else
+            {
+                Image ctrl = (Image)wrapPanel_main.Children[0];
+                int rowItemCount = (int)(newWidth / ctrl.Width);
+                int rowCount = wrapPanel_main.Children.Count / rowItemCount + ((wrapPanel_main.Children.Count % rowItemCount) > 0 ? 1 : 0);
+                wrapPanel_main.Height = (ctrl.Height * rowCount) + 30;
+            }
+        }
+
+
+        private Class_booker booker;
+        private void ReloadPictures()
+        {
+            booker = new Class_booker(new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory + "\\Book"));
+            booker.CatalogEntry_Clicked += Booker_CatalogEntry_Clicked;
+
+            stackPanel_catalog.Children.Clear();
+            foreach (Class_booker.CatalogEntry ce in booker.catalog)
+            {
+                stackPanel_catalog.Children.Add(ce.button);
+            }
+        }
+
+        private void Booker_CatalogEntry_Clicked(object sender, Class_booker.CatalogEntry_ClickedArgs e)
+        {
+            wrapPanel_main.Children.Clear();
+            foreach (Class_booker.CatalogEntry.gunEntry ge in e.targetCatalogEntry.guns)
+            {
+                wrapPanel_main.Children.Add(ge.image);
+            }
+
+
+
+            ReSize_wrapPanel_main(new Size(scrollViewer_main.Width, scrollViewer_main.Height));
+        }
+    }
+}
